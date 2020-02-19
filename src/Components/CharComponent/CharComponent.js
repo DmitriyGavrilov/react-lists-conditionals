bc353eff27e9b50ef75cc4d340ef8cc37ed80693
Dmitrii <never.forget89@gmail.com>
2020-02-19 19:08:24 +0300

    change != to !==

--- src/Components/CharComponent/CharComponent.js
+++ src/Components/CharComponent/CharComponent.js
@@ -3,9 +3,9 @@ import './CharComponent.css';
 
 const CharComponent = (props) => {
     return (<div>    
-                {props.arrofchar != null ? props.arrofchar.map((word) => {
+                {props.arrofchar !== null ? props.arrofchar.map((word) => {
                     // console.log(word);
-                    return word.text != '' ? <div key={word.id} onClick={props.deletechar} className="CharComponent">{word.text}</div> : null;
+                    return word.text !== '' ? <div key={word.id} onClick={props.deletechar} className="CharComponent">{word.text}</div> : null;
                 }) : null}
             </div>);
 };
